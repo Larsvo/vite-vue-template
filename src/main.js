235a776af2013abe11cfddf08c5d7eec892eff37
larsvo@involvit.nl <larsvo@involvit.nl>
2025-03-21 14:34:17 +0100

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+// main.js or main.ts
+import { createApp } from 'vue';
+import App from './App.vue';
+// import router from './router'; // Assuming you have a router configuration file
+
+const app = createApp(App);
+// app.use(router); // Register the router plugin
+app.mount('#app');
